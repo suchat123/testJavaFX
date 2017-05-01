e15ee029e56a48e0d4575453b0dfa0726def98f7
suchat <poyxml@hotmail.com>
2017-05-01 09:10:46 +0700

    FirstJavaFX

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by suchat on 1/5/2560.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
